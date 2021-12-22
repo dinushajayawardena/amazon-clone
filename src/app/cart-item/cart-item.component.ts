a8b58d739cfc5ea6328b94a834941e944e5a5adc
Dinusha Jayawardena <dinusha.g.jayawardena@gmail.com>
2021-12-22 12:48:15 +0530

    add basic template to items

--- src/app/cart-item/cart-item.component.ts
+++ src/app/cart-item/cart-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Item} from "../dto/item";
 
 @Component({
   selector: 'app-cart-item',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartItemComponent implements OnInit {
 
-  constructor() { }
+  item = new Item("I001",
+    "https://m.media-amazon.com/images/I/71+aeM5xpZL._AC_UY218_.jpg",
+    "Donerton Gaming Headset, Over-Ear Gaming Headphones with Noise Canceling Mic, Stereo Bass Surround Sound, Soft Memory Earmuffs LED Light PS4 Gaming Headset Compatible with PC, Laptop, PS4, PS5, Red,",
+    4,
+    99,
+    10)
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
